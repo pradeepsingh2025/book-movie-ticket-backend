5053c9e8e4d3700401334bc0cc2c795f796b8069
pradeepsingh2025 <pradeepsinghsolanki36@gmail.com>
2026-01-19 23:03:36 +0530

    feat: Add initial Spring Boot application class with Dotenv integration for environment variables.

--- src/main/java/me/riazulislam/infinitecineplexbackend/InfiniteCineplexBackendApplication.java
+++ src/main/java/me/riazulislam/infinitecineplexbackend/InfiniteCineplexBackendApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class InfiniteCineplexBackendApplication {
 
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.configure().load();
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
         dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 
         SpringApplication.run(InfiniteCineplexBackendApplication.class, args);
